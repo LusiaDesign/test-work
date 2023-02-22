f33f778bcbae3d0841d624810adec3ecffacf187
LusiaDesign <94785609+LusiaDesign@users.noreply.github.com>
2023-02-22 15:18:31 +0300

    Delete app directory

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('.menu-btn').on(('click'), function () {
-        $('.sidebar').toggleClass('active');
-        $('.menu-btn__btn-line').toggleClass('active');
-        $('body').toggleClass('no-scroll');
-    });
-})
